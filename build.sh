26d55b9ff844f4508f177e1518cc4d0721724d39
赵伟 <zhaowei@DZW-2.local>
2021-05-11 18:50:24 +0800

    Seperate build process into 2 phases, phase 1: configure&make; phase 2: make install Committer: winter zhang

--- build.sh
+++ build.sh
@@ -1,43 +1,70 @@
 #! /bin/bash
 # configure and build Kunlun computing node program.
-# If you don't need to configure it, simply do make install in ./src.
-# usage: build.sh <build-type> <install-dir>
+# If you don't need to configure it, simply do make install.
+# usage: build.sh <build-type> <prefix> | install
 # build-type: Release or Debug
 # e.g. bash build.sh Release /home/dzw/mysql_installs/postgresql-11.5-rel
-if test $# != 2 || test $1 != "Debug" -a $1 != "Release"; then
-	echo "Usage: build.sh <build-type> <install-dir> \n\tbuild-type: Release or Debug"
-	exit 1
-fi
-
-mkdir -p $2
-
 export SRCROOT=`pwd`
-cd $SRCROOT/resources
-test -d mysql || tar -xzf mysql_client.tgz
-ln -s ./libmariadb.so.3 libmariadb.so
-cd $SRCROOT/src/include/sharding
-ln -s $SRCROOT/resources/mysql mysql
-cd $SRCROOT
+action="$1"
 
-export LIBS=-lmariadb
-export LDFLAGS=-L$SRCROOT/resources
+usage(){
+	echo "Usage: build.sh <build-type> <prefix> | install"
+	echo "	build-type: Debug | Release"
+	exit 1
+}
 
-if test $1 = "Release"; then
-	export CFLAGS="-O2 -ggdb3"
-	export CXXFLAGS="-O2 -ggdb3"
-	./configure --with-openssl --with-icu --prefix=$2 
-	# ./configure --with-openssl --with-icu --with-llvm  --prefix=$2
-elif test $1 = "Debug"; then
+init() {
+	test "$1" = "" && usage
+	cd $SRCROOT
+	idir="$1"
+	echo "$idir" > installdir
+	cd $SRCROOT/resources
+	test -d mysql || tar -xzf mysql_client.tgz
+	# ln -s ./libmariadb.so.3 libmariadb.so
+	cd $SRCROOT/src/include/sharding
+	ln -s $SRCROOT/resources/mysql mysql
+	cd $SRCROOT
+}
 
-	export CFLAGS="-O0 -ggdb3 -DENABLE_DEBUG"
-	export CXXFLAGS="-O0 -ggdb3 -DENABLE_DEBUG "
-	./configure --with-openssl --with-icu --enable-debug --enable-cassert   --prefix=$2
-	# ./configure --with-openssl --with-icu --with-llvm  --enable-debug --enable-cassert   --prefix=$2
-	# in current ubuntu-20.04 latest llvm there is build error.
+build() {
+	idir="$2"
+	init "$idir"
+	export LIBS=-lmariadb
+	export LDFLAGS=-L$SRCROOT/resources
+	if test "$1" = "Release"; then
+		export CFLAGS="-O2 -ggdb3"
+		export CXXFLAGS="-O2 -ggdb3"
+		./configure --with-openssl --with-icu --prefix="$idir"
+		# ./configure --with-openssl --with-icu --with-llvm  --prefix="$idir"
+	elif test "$1" = "Debug"; then
+		export CFLAGS="-O0 -ggdb3 -DENABLE_DEBUG"
+		export CXXFLAGS="-O0 -ggdb3 -DENABLE_DEBUG "
+		./configure --with-openssl --with-icu --enable-debug --enable-cassert   --prefix="$idir"
+		# ./configure --with-openssl --with-icu --with-llvm  --enable-debug --enable-cassert   --prefix="$idir"
+		# in current ubuntu-20.04 latest llvm there is build error.
+	else #defensive
+		usage
+	fi
+	make
+}
+
+install() {
+	test -f installdir || {
+		echo "installation directory is not set, call 'build.sh build' first!"
+		usage
+	}
+	idir=`cat installdir`
+	mkdir -p "$idir"
+	make install
+	bash ./post-install.sh "$idir"
+}
+
+if test "$action" = "Debug" -o "$action" = "Release"; then
+	test "$#" != 2 && usage
+	build "$1" "$2"
+elif test "$action" = "install"; then
+	test "$#" != 1 && usage
+	install
+else
+	usage
 fi
-
-cd $SRCROOT/src
-make clean
-make install -j8 2> err.txt
-cd $SRCROOT
-bash ./post-install.sh $2
--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ then
     exit 0;
 fi
 
-cp ReleaseNotes.txt INSTALL.KunLun README.KunLun ./昆仑分布式数据库简介.txt  $1
+cp ReleaseNotes.md INSTALL.kunlun.md README.md ./昆仑分布式数据库简介.md  $1
 mkdir -p $1/scripts
 cd scripts/
 cp add_comp_nodes.py  add_shards.py  bootstrap.py  common.py comp-nodes.json  install_pg.py start_pg.py shards-config.json create_cluster.py  meta_inuse.sql  meta-shard.json $1/scripts
@@ -18,7 +18,7 @@ mkdir -p deps
 
 #copy dependent shared object libs to lib/deps
 cd $1/bin
-ls | xargs ldd >> ./prog-deps.txt
+ls | grep -v 'prog-deps.txt' | xargs ldd >> ./prog-deps.txt
 cat ./prog-deps.txt | sed -n '/^.* => .*$/p' | sed  's/^.* => \(.*\)(.*$/\1/g' | sort | uniq | sed /^.*postgresql-11\.5.*$/d | while read f ; do  cp $f $1/lib/deps ; done
 rm ./prog-deps.txt
 

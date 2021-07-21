8c148230d31496033c9a8e31658fd76fdd9e2fed
winter.zhang@live.com <winter.zhang@live.com>
2021-07-21 10:15:42 +0800

    Fix the post-install.sh error after changing the cn readme file.

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ then
     exit 0;
 fi
 
-cp ReleaseNotes.md INSTALL.kunlun.md README.md ./昆仑分布式数据库简介.md  $1
+cp ReleaseNotes.md INSTALL.kunlun.md README.md README_CN.md  $1
 mkdir -p $1/scripts
 cd scripts/
 cp add_comp_nodes.py  add_shards.py  bootstrap.py  common.py comp-nodes.json  install_pg.py start_pg.py shards-config.json create_cluster.py  meta_inuse.sql  meta-shard.json $1/scripts

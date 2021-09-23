5f0d32fb2cc50cfa67abad9fcc1545a6a4a01125
winter.zhang@live.com <winter.zhang@live.com>
2021-09-23 15:05:03 +0800

    Fix build issues on Centos7

--- post-install.sh
+++ post-install.sh
@@ -9,16 +9,22 @@ mkdir -p $1/scripts
 cd scripts/
 cp add_comp_nodes.py  add_shards.py  bootstrap.py  common.py comp-nodes.json  install_pg.py start_pg.py shards-config.json create_cluster.py  meta_inuse.sql  meta-shard.json $1/scripts
 cp -r ../resources $1
-cp ../resources/libmariadb.so.3 $1/lib
+cp -f ../resources/libmariadb.so.3 $1/lib
+cp -f ../resources/libssl.so.1.1 $1/lib
+cp -f ../resources/libcrypto.so.1.1 $1/lib
 cd $1/lib
-ln -s ./libmariadb.so.3  libmysqlclient.so
-ln -s ./libmariadb.so.3  libmariadb.so
+rm -f libmysqlclient.so && ln -s ./libmariadb.so.3  libmysqlclient.so
+rm -f libmariadb.so && ln -s ./libmariadb.so.3  libmariadb.so
+rm -f libssl.so && ln -s ./libssl.so.1.1 libssl.so
+rm -f libcrypto.so && ln -s ./libcrypto.so.1.1 libcrypto.so
 
 mkdir -p deps
 
 #copy dependent shared object libs to lib/deps
+export LD_LIBRARY_PATH=$1/lib:$LD_LIBRARY_PATH
 cd $1/bin
+rm -f ./prog-deps.txt
 ls | grep -v 'prog-deps.txt' | xargs ldd >> ./prog-deps.txt 2>/dev/null
-cat ./prog-deps.txt | sed -n '/^.* => .*$/p' | sed  's/^.* => \(.*\)(.*$/\1/g' | sort | uniq | sed /^.*postgresql-11\.5.*$/d | while read f ; do  cp $f $1/lib/deps ; done
+cat ./prog-deps.txt | sed -n '/^.* => .*$/p' | sed  's/^.* => \(.*\)(.*$/\1/g' | sort | uniq | sed /^.*postgresql-11\.5.*$/d | sed '/^ *$/d' |  while read f ; do  cp $f $1/lib/deps ; done
 rm ./prog-deps.txt
 

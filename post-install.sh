b09d62a456e417df54865b47f66ed388352cc613
winter.zhang@live.com <winter.zhang@live.com>
2021-10-26 12:10:30 +0800

    fix post-install.sh issue.

--- post-install.sh
+++ post-install.sh
@@ -4,11 +4,12 @@ then
     exit 0;
 fi
 
-cp ReleaseNotes.md INSTALL.kunlun.md README.md README_CN.md  $1
+cp -f ReleaseNotes.md INSTALL.kunlun.md README.md README_CN.md  $1
 mkdir -p $1/scripts
 cd scripts/
-cp add_comp_nodes.py  add_shards.py  bootstrap.py  common.py comp-nodes.json  install_pg.py start_pg.py shards-config.json create_cluster.py  meta_inuse.sql  meta-shard.json $1/scripts
-cp -r ../resources $1
+cp -f add_comp_nodes.py add_comp_self.py add_shards.py  bootstrap.py  common.py install_pg.py start_pg.py create_cluster.py \
+	meta_inuse.sql comp-nodes.json shards-config.json  meta-shard.json $1/scripts
+cp -fr ../resources $1
 builddir="`pwd`"
 cd $1
 test ! -e lib && test -d lib64 && ln -s lib64 lib
@@ -28,7 +29,7 @@ rm -f ./prog-deps.txt
 ls | grep -v 'prog-deps.txt' | xargs ldd >> ./prog-deps.txt 2>/dev/null
 cat ./prog-deps.txt | sed -n '/^.* => .*$/p' | sed  's/^.* => \(.*\)(.*$/\1/g' | sort | uniq | sed /^.*postgresql-11\.5.*$/d | sed '/^ *$/d' |  while read f ; do
 	echo "install $f to lib/deps"
-	cp $f $1/lib/deps
+	cp -f $f $1/lib/deps
 done
 rm ./prog-deps.txt
 

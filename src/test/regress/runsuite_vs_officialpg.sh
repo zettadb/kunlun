b432cf62a1e5438d8f22ba8943c0ebfb40fe4aa0
winter.zhang@live.com <winter.zhang@live.com>
2021-12-20 16:32:18 +0800

    [#336] Save all the test output for later check, instead of having to run the test again.

--- src/test/regress/runsuite_vs_officialpg.sh
+++ src/test/regress/runsuite_vs_officialpg.sh
@@ -7,6 +7,11 @@ cat serial_schedule | grep -v '^#' | sed '/^[ 	]*$/d' | awk '{print $2}' | while
 		echo "Running sql/$f.sql ......"
 		bash run.sh sql/$f.sql
 		mv out.diff $f.diff
+		mv out.diff.orig $f.diff.orig
+		mv 1.out $f.out1
+		mv 1.out.p $f.out1.p
+		mv 2.out $f.out2
+		mv 2.out.p $f.out2.p
 		echo "======= diff content for $f.diff =========="
 		cat $f.diff
 	fi

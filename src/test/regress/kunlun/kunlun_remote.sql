e5415ae2827ba5498ce516ead7f7dac686a416ca
vito4172 <vito@zettadb.com>
2022-02-23 15:05:44 +0800

    test set up on kunlun

---
+++ src/test/regress/kunlun/kunlun_remote.sql
@@ -0,0 +1,242 @@
+
+drop table if exists t4;
+drop table if exists t5;
+drop table if exists t6;
+
+create table t4(a int primary key, b int unique) with oids;
+create table t4(a int primary key, b int unique, c varchar(32) not null);
+show last_remote_sql;
+create index t4_cb on t4(c desc, b);
+show last_remote_sql;
+\d+ t4;
+\d+ t4_cb;
+
+-- foreign keys are forbidden
+create table t5(a int references t4(a));
+
+-- temp table exits on computing node.
+create temporary table t6(a int);
+insert into t6 values(1),(2),(3);
+select*from t6;
+
+drop table if exists t7;
+create table t7(a int, b varchar(16), c int not null, d varchar(16) not null, primary key(c,d),unique(a,c),unique(b,d));
+show last_remote_sql;
+create index t7_abc on t7(b desc, a asc, c desc);
+show last_remote_sql;
+\d+ t7;
+\d+ t7_abc;
+
+-- forbidden create index clauses start
+create index t7_b on t7(b collate en_US);
+create index t7_b on t7(b collate zh_CN);
+
+create index t7_b on t7(b ) include (c);
+create index t7_b on t7(b zh_pinyin_cmp);
+create index t7_b on t7(b zh_stroke_cmp);
+create index t7_a on t7((a+c));
+create index t7_a on t7 (a) where a between 100 and 200;
+
+create index t7_b on t7(b nulls first);
+drop index t7_b;
+create index t7_b on t7(b nulls last);
+show last_remote_sql;
+\d+ t7_b;
+
+-- forbidden create index clauses end
+
+
+drop table if exists t3;
+create table t3(a int) partition by list(a);
+show last_remote_sql;
+\d+ t3;
+
+create table t301 partition of t3 for values in (1,3,5);
+show last_remote_sql;
+\d+ t301;
+create table t302 partition of t3 for values in (2,4,6);
+show last_remote_sql;
+\d+ t302;
+
+drop table if exists t3;
+create table t3(a int, b date, c timestamp, d time, e varchar(64), primary key(b,d)) partition by range(b);
+create unique index on t3(c, b);
+create index on t3(c);
+show last_remote_sql;
+\d+ t3;
+
+create table t301 partition of t3 for values from ('2000-01-01') to ('2009-12-31');
+show last_remote_sql;
+\d+ t301;
+\d+ t3;
+
+create table t303 partition of t3 for values from ('2020-01-01') to ('2029-12-31');
+show last_remote_sql;
+\d+ t303;
+\d+ t3;
+
+-- create a new index t3_b_a_idx , existing leaf partitions should be given t3_b_a_idx and t3_a_idx.
+create unique index t3_b_a_idx on t3(b, a);
+show last_remote_sql;
+create index t3_a_idx on t3(a);
+show last_remote_sql;
+\d+ t301;
+\d+ t303;
+\d+ t3;
+
+-- new leaf partitions should have t3_b_a_idx in 'create table' stmt, and also an independent
+-- 'create index t3_a_idx' stmt, because in pg non-unique index can't be created in 'create table' stmt.
+create table t302 partition of t3 for values from ('2010-01-01') to ('2019-12-31');
+show last_remote_sql;
+\d+ t3;
+\d+ t302;
+\d+ t301;
+\d+ t303;
+
+create table t304 partition of t3 for values from ('2030-01-01') to ('2049-12-31');
+show last_remote_sql;
+\d+ t3;
+\d+ t304;
+\d+ t301;
+\d+ t302;
+\d+ t303;
+
+-- create a new idx for t302 only.
+create unique index t302_b_e_idx on t302(b,e);
+show last_remote_sql;
+\d+ t302;
+\d+ t3;
+
+-- create a new idx for t304 only.
+create index t304_b_e_idx on t304(b,e);
+show last_remote_sql;
+\d+ t304;
+\d+ t3;
+
+-- create another index on t3, existing leaf partitions should all be given
+-- this idx. only the last 'create index' can be printed here.
+create index t3_e_idx on t3(e);
+show last_remote_sql;
+\d+ t301;
+\d+ t302;
+\d+ t303;
+\d+ t304;
+\d+ t3;
+insert into t3 values(2, '2025-01-16 11:25:09', '2015-01-16 11:25:09', '2015-01-16 11:25:09', 'abc'),(3, '2015-01-16 11:25:09', '2020-01-16 11:25:09', '2015-01-16 11:25:09', 'def'), (4, '2000-01-16 11:25:09', '2020-01-16 11:25:10', '2015-01-16 11:25:09', 'xyz'),(5, '2035-01-16 11:25:09', '2020-01-16 11:25:11', '2015-01-16 11:25:09', 'spider');
+
+
+drop table if exists t1;
+create table t1(a int, b int) partition by hash(a,b);
+show last_remote_sql;
+\d+ t1;
+create table t100 partition of t1 for values with (modulus 4, remainder 0);
+show last_remote_sql;
+create table t101 partition of t1 for values with (modulus 4, remainder 1);
+show last_remote_sql;
+create table t102 partition of t1 for values with (modulus 4, remainder 2);
+show last_remote_sql;
+create table t103 partition of t1 for values with (modulus 4, remainder 3);
+show last_remote_sql;
+
+
+drop table if exists t2;
+create table t2(a int, b int, c varchar(32), d char(16), primary key(a,b), unique(b,c)) partition by hash(b);
+show last_remote_sql;
+\d+ t2;
+create table t200 partition of t2 for values with (modulus 4, remainder 0);
+show last_remote_sql;
+create table t201 partition of t2 for values with (modulus 4, remainder 1);
+show last_remote_sql;
+create table t202 partition of t2 for values with (modulus 4, remainder 2);
+show last_remote_sql;
+create table t203 partition of t2 for values with (modulus 4, remainder 3);
+show last_remote_sql;
+
+set log_min_messages=debug5;
+create index t2_c_d on t2(d desc, c);
+
+-- only the last index's remote sql is printed,others are overwritten, but
+-- it's OK, this is only for debugging. all remote sql will be sent to remote
+-- targets properly. debug5 is turned on so you can check the log file for all
+-- four statements generated by above 'create index' stmt.
+
+show last_remote_sql;
+\d+ t2_c_d;
+\d+ t200_d_c_idx;
+\d+ t201_d_c_idx;
+\d+ t202_d_c_idx;
+\d+ t203_d_c_idx;
+
+
+drop table if exists t1;
+create table t1(a varchar(30), b int, c date, d time, e timestamp, f char(20), primary key(a,b),unique(e,b)) partition by hash(b);
+show last_remote_sql;
+create index t1_f on t1(f);
+show last_remote_sql;
+\d+ t1;
+create table t100 partition of t1 for values with (modulus 4, remainder 0);
+show last_remote_sql;
+
+create table t101 partition of t1 for values with (modulus 4, remainder 1);
+show last_remote_sql;
+
+create unique index t1_d_b on t1(d,b desc);
+show last_remote_sql;
+create index t1_d on t1(d desc);
+show last_remote_sql;
+
+create table t102 partition of t1 for values with (modulus 4, remainder 2);
+show last_remote_sql;
+create index t102_a_b on t102(a desc, b);
+show last_remote_sql;
+
+create table t103 partition of t1 for values with (modulus 4, remainder 3);
+show last_remote_sql;
+create unique index t103_a_b on t102(c, b desc, e desc);
+show last_remote_sql;
+
+
+drop table if exists t2;
+create table t2(a int, b timestamptz, c timestamp, d money, e numeric(7,4), f real, g double precision, primary key(a,b), unique(a,b,c)) partition by list(a);
+show last_remote_sql;
+\d+ t2;
+create table t200 partition of t2 for values in (1,2,3,4);
+show last_remote_sql;
+\d+ t200;
+create index t2_d_g on t2(d,g desc);
+show last_remote_sql;
+\d+ t2;
+\d+ t200;
+
+create table t201 partition of t2 for values in (5,6,7);
+show last_remote_sql;
+\d+ t201;
+create unique index t2_c_d on t2(a,c,d desc);
+show last_remote_sql;
+\d+ t2;
+\d+ t200;
+\d+ t201;
+
+create unique index t201_c_a on t201(c,a desc);
+show last_remote_sql;
+\d+ t201;
+
+create table t202 partition of t2 for values in (8,9,10)
+show last_remote_sql;
+\d+ t202;
+create index on t2(b,e);
+show last_remote_sql;
+create index on t2(g desc,f);
+show last_remote_sql;
+\d+ t2;
+\d+ t201;
+\d+ t202;
+
+create table t10 (a decimal(66));
+create table t10 (a decimal(10,31));
+
+create table t10(a decimal, b decimal(12), c decimal(20, 10), d real, e float(10), f float(30), g bit(11), h money, i timestamptz, j timestamp(6), k time(6), l date,primary key(a), unique(b,d), unique(g,j), unique(c,k));
+create unique index t10ei on t10(e,i);
+create index t10fh on t10(f,h,k);
+
+
---
+++ src/test/regress/kunlun/kunlun_remote_dml.sql
@@ -0,0 +1,330 @@
+drop table if exists t1;
+
+create table t1(a int primary key, b serial) partition by hash(a);
+create table t100 partition of t1 for values with (modulus 4, remainder 0); 
+create table t101 partition of t1 for values with (modulus 4, remainder 1); 
+create table t102 partition of t1 for values with (modulus 4, remainder 2); 
+create table t103 partition of t1 for values with (modulus 4, remainder 3); 
+insert into t1 values(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20);
+select*from t1;
+select a from t1 where a between 3 and 11;
+select * from t1 where a in (3,7,11, 13);
+update t1 set a=a+1 where a > 1 and a < 5;
+delete from t1 where a %5=0;
+select * from t1 where a %5=0;
+select*from t1;
+
+start transaction;
+delete from t1 where a > 10;
+select*from t1;
+create table tfail(a int);
+delete from t1;
+select*from t1;
+commit;
+
+start transaction;
+delete from t1;
+select*from t1;
+rollback;
+select*from t1;
+
+
+
+create table t5(a int primary key, b timestamptz default now(), c varchar(32) default 'abc') partition by range(a);
+create table t501 partition of t5 for values from (MINVALUE) to (10);
+create table t502 partition of t5 for values from (10) to (20);
+create table t503 partition of t5 for values from (20) to (30);
+create table t504 partition of t5 for values from (30) to (MAXVALUE);
+insert into t5 values(-10),(0), (15),(40) returning *;
+insert into t5 values(-20),(10), (25),(400);
+
+PREPARE q1(int, int) AS SELECT * FROM t5 WHERE a between $1 and $2;
+
+EXECUTE q1(-100, 20);
+EXECUTE q1(0, 40);
+
+
+PREPARE q2(int, int) AS SELECT * FROM t1 WHERE a between $1 and $2;
+
+EXECUTE q2(0, 10);
+EXECUTE q1(10, 30);
+EXECUTE q2(5, 40);
+deallocate q1;
+
+create table t4(a int primary key, b serial);
+insert into t4 values(1),(2),(3),(4),(5),(6),(7),(8),(9),(10),(11),(12),(13),(14),(15),(16),(17),(18),(19),(20);
+
+PREPARE q3(int, int) AS SELECT * FROM t4 WHERE a between $1 and $2;
+
+
+EXECUTE q3(6, 16);
+
+EXECUTE q1(0, 30);
+EXECUTE q2(5, 10);
+deallocate q2;
+
+PREPARE q4(int, int) as update t1 set b=$1 where a=$2;
+select*from t1;
+EXECUTE q4(10, 9);
+EXECUTE q3(0, 40);
+EXECUTE q4(11, 10);
+
+begin;
+EXECUTE q4(14, 13);
+EXECUTE q4(12, 11);
+select*from t1 where a between 9 and 12;
+commit;
+
+drop table t1;
+drop table t4;
+drop table t5;
+
+-- bug 30
+drop table if exists uv_iocu_tab cascade;
+create table uv_iocu_tab (a serial primary key, b varchar(50));
+insert into uv_iocu_tab (b) values('abc'),('bcd'),('xyz');
+create view uv_iocu_view as select b as bb, a as aa, uv_iocu_tab::varchar(50) as cc from uv_iocu_tab;
+select * from uv_iocu_view;
+drop view uv_iocu_view;
+create view uv_iocu_view as select a as aa, b as bb, uv_iocu_tab::varchar(50) as cc from uv_iocu_tab;
+select * from uv_iocu_view;
+
+
+-- bug 31
+drop table if exists base_tbl cascade;
+CREATE TABLE base_tbl(id serial primary key, a float);
+INSERT INTO base_tbl (a) SELECT i/10.0 FROM generate_series(1,10) g(i);
+CREATE VIEW rw_view1 AS SELECT ctid, sin(a) s, a, cos(a) c FROM base_tbl WHERE a != 0 ORDER BY abs(a);
+select*from rw_view1;
+drop view rw_view1;
+CREATE VIEW rw_view1 AS SELECT sin(a) s, a, cos(a) c FROM base_tbl WHERE a != 0 ORDER BY abs(a);
+select*from rw_view1;
+INSERT INTO rw_view1 (a) VALUES (1.1) RETURNING a, s, c;
+select*from rw_view1;
+
+-- bug 39
+drop table if exists SUBSELECT_TBL cascade;
+CREATE TABLE SUBSELECT_TBL ( id serial primary key, f1 integer, f2 integer, f3 float );
+INSERT INTO SUBSELECT_TBL (f1, f2, f3) VALUES (1, 2, 3), (2, 3, 4), (3, 4, 5), (1, 1, 1), (2, 2, 2), (3, 3, 3), (6, 7, 8), (8, 9, NULL);
+SELECT f1 AS "Correlated Field", f2 AS "Second Field" FROM SUBSELECT_TBL upper WHERE f1 IN (SELECT f2 FROM SUBSELECT_TBL WHERE f1 = upper.f1);
+
+create temp table rngfunc(f1 int8, f2 int8);
+create function testrngfunc() returns record as $$
+
+    insert into rngfunc values (1,2) returning *;
+
+$$ language sql;
+select testrngfunc();
+select * from testrngfunc() as t(f1 int8,f2 int8);
+select * from testrngfunc() as t(f1 int8,f2 int8);
+
+-- bug 66 
+DROP TABLE if exists prt1 cascade;
+DROP TABLE if exists prt2 cascade;
+CREATE TABLE prt1 (a int primary key, b int, c varchar) PARTITION BY RANGE(a);
+CREATE TABLE prt1_p1 PARTITION OF prt1 FOR VALUES FROM (0) TO (250);
+CREATE TABLE prt1_p3 PARTITION OF prt1 FOR VALUES FROM (500) TO (600);
+CREATE TABLE prt1_p2 PARTITION OF prt1 FOR VALUES FROM (250) TO (500);
+INSERT INTO prt1 SELECT i, i % 25, to_char(i, 'FM0000') FROM generate_series(0, 599) i WHERE i % 2 = 0;
+CREATE TABLE prt2 (a int, b int primary key, c varchar) PARTITION BY RANGE(b);
+CREATE TABLE prt2_p1 PARTITION OF prt2 FOR VALUES FROM (0) TO (250);
+CREATE TABLE prt2_p2 PARTITION OF prt2 FOR VALUES FROM (250) TO (500);
+CREATE TABLE prt2_p3 PARTITION OF prt2 FOR VALUES FROM (500) TO (600);
+INSERT INTO prt2 SELECT i % 25, i, to_char(i, 'FM0000') FROM generate_series(0, 599) i WHERE i % 3 = 0;
+
+SELECT t1, t2 FROM prt1 t1 LEFT JOIN prt2 t2 ON t1.a = t2.b WHERE t1.b = 0 ORDER BY t1.a, t2.b;
+EXPLAIN(verbose)
+SELECT t1, t2 FROM prt1 t1 LEFT JOIN prt2 t2 ON t1.a = t2.b WHERE t1.b = 0 ORDER BY t1.a, t2.b;
+SELECT t1.a, t2.b FROM prt1 t1, prt2 t2 WHERE t1::text = t2::text AND t1.a = t2.b ORDER BY t1.a;
+EXPLAIN(verbose)
+SELECT t1.a, t2.b FROM prt1 t1, prt2 t2 WHERE t1::text = t2::text AND t1.a = t2.b ORDER BY t1.a;
+SELECT * FROM prt1 t1 LEFT JOIN  (SELECT t2.a AS t2a, t3.a AS t3a, least(t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+EXPLAIN(verbose)
+SELECT * FROM prt1 t1 LEFT JOIN  (SELECT t2.a AS t2a, t3.a AS t3a, least(t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+SELECT * FROM prt1 t1 LEFT JOIN LATERAL (SELECT t2.a AS t2a, t3.a AS t3a, least(t1.a,t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+
+EXPLAIN(verbose)
+SELECT * FROM prt1 t1 LEFT JOIN LATERAL (SELECT t2.a AS t2a, t3.a AS t3a, least(t1.a,t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+
+SET enable_partitionwise_join to true;
+SELECT t1, t2 FROM prt1 t1 LEFT JOIN prt2 t2 ON t1.a = t2.b WHERE t1.b = 0 ORDER BY t1.a, t2.b;
+EXPLAIN(verbose)
+SELECT t1, t2 FROM prt1 t1 LEFT JOIN prt2 t2 ON t1.a = t2.b WHERE t1.b = 0 ORDER BY t1.a, t2.b;
+SELECT t1.a, t2.b FROM prt1 t1, prt2 t2 WHERE t1::text = t2::text AND t1.a = t2.b ORDER BY t1.a;
+EXPLAIN(verbose)
+SELECT t1.a, t2.b FROM prt1 t1, prt2 t2 WHERE t1::text = t2::text AND t1.a = t2.b ORDER BY t1.a;
+SELECT * FROM prt1 t1 LEFT JOIN  (SELECT t2.a AS t2a, t3.a AS t3a, least(t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+EXPLAIN(verbose)
+SELECT * FROM prt1 t1 LEFT JOIN  (SELECT t2.a AS t2a, t3.a AS t3a, least(t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+SELECT * FROM prt1 t1 LEFT JOIN LATERAL (SELECT t2.a AS t2a, t3.a AS t3a, least(t1.a,t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+
+EXPLAIN(verbose)
+SELECT * FROM prt1 t1 LEFT JOIN LATERAL (SELECT t2.a AS t2a, t3.a AS t3a, least(t1.a,t2.a,t3.b) FROM prt1 t2 JOIN prt2 t3 ON (t2.a = t3.b)) ss ON t1.a = ss.t2a WHERE t1.b = 0 ORDER BY t1.a;
+
+
+-- bug 71
+DROP table if exists T;
+CREATE TABLE T(pk INT NOT NULL PRIMARY KEY);
+INSERT INTO T VALUES (1);
+ALTER TABLE T ADD COLUMN c1 TIMESTAMP DEFAULT now();
+select*from T;
+insert into T values(2);
+select*from T;
+
+ CREATE OR REPLACE FUNCTION foo(a INT) RETURNS TEXT AS $$
+DECLARE res TEXT = 'xyz';
+
+    i INT;
+
+BEGIN
+
+    i = 0;
+    WHILE (i < a) LOOP
+
+        res = res || chr(ascii('a') + i);
+
+        i = i + 1;
+
+    END LOOP;
+    RETURN res;
+
+END; $$ LANGUAGE PLPGSQL STABLE;
+DROP table if exists T;
+CREATE TABLE T(pk INT NOT NULL PRIMARY KEY, c_int INT DEFAULT LENGTH(foo(6)));
+INSERT INTO T VALUES (1), (2);
+select*from t;
+ALTER TABLE T ADD COLUMN c_bpchar BPCHAR(50) DEFAULT foo(4), ALTER COLUMN c_int SET DEFAULT LENGTH(foo(8));
+select*from t;
+
+
+
+-- bug 65
+drop table if exists itest13 cascade;
+CREATE TABLE itest13 (a int primary key);
+ALTER TABLE itest13 ADD COLUMN b int GENERATED BY DEFAULT AS IDENTITY;
+INSERT INTO itest13 VALUES (1), (2), (3);
+SELECT * FROM itest13;
+ALTER TABLE itest13 ADD COLUMN c int GENERATED BY DEFAULT AS IDENTITY;
+SELECT * FROM itest13;
+
+drop table if exists itest6 cascade;
+CREATE TABLE itest6 (a int GENERATED ALWAYS AS IDENTITY primary key, b text);
+INSERT INTO itest6 DEFAULT VALUES;
+ALTER TABLE itest6 ALTER COLUMN a SET GENERATED BY DEFAULT SET INCREMENT BY 2 SET START WITH 100 RESTART;
+SELECT * FROM itest6;
+INSERT INTO itest6 DEFAULT VALUES;
+INSERT INTO itest6 DEFAULT VALUES;
+SELECT * FROM itest6;
+
+
+-- bug 68
+drop table  if exists t2 cascade;
+create table t2(a int);
+create index on t2(a,a);
+
+
+-- bug 13
+SELECT SESSION_USER, CURRENT_USER;
+drop schema if exists testschema cascade;
+CREATE SCHEMA if not exists testschema;
+SELECT SESSION_USER, CURRENT_USER;
+CREATE TABLE testschema.foo (i serial primary key, j serial);
+insert into testschema.foo default values;
+insert into testschema.foo default values;
+select*from  testschema.foo;
+create sequence testschema.seq1;
+select testschema.seq1.nextval, nextval('testschema.seq1');
+drop schema testschema cascade;
+
+-- bug 78
+create table b1(v box);
+
+-- bug 50 THIS BUG Is postponed
+create user user1;
+drop table if exists r1 cascade;
+SET SESSION AUTHORIZATION user1;
+
+SET row_security = on;
+CREATE TABLE r1 (a int primary key);
+INSERT INTO r1 VALUES (10), (20);
+CREATE POLICY p1 ON r1 USING (false);
+ALTER TABLE r1 ENABLE ROW LEVEL SECURITY;
+ALTER TABLE r1 FORCE ROW LEVEL SECURITY;
+TABLE r1;
+INSERT INTO r1 VALUES (1);
+UPDATE r1 SET a = 1;
+DELETE FROM r1;
+SET SESSION AUTHORIZATION abc;
+
+-- bug 84
+drop table if exists collate_test10 cascade;
+ CREATE TABLE collate_test10 (
+
+    a int primary key,
+    x varchar(50) COLLATE "C",
+    y varchar(50) COLLATE "POSIX"
+
+);
+INSERT INTO collate_test10 VALUES (1, 'hij', 'hij'), (2, 'HIJ', 'HIJ');
+select x < y from collate_test10;
+
+drop table if exists collate_test1 cascade;
+drop table if exists collate_test2 cascade;
+CREATE TABLE collate_test1 (id serial primary key, a int, b varchar(50) COLLATE "C" NOT NULL);
+CREATE TABLE collate_test2 (id serial primary key,  a int, b varchar(50) COLLATE "POSIX" );
+INSERT INTO collate_test1 (a,b) VALUES (1, 'abc'), (2, 'Abc'), (3, 'bbc'), (4, 'ABD');
+INSERT INTO collate_test2 (a,b) SELECT a,b FROM collate_test1;
+SELECT a, b FROM collate_test2 WHERE a < 4 INTERSECT SELECT a, b FROM collate_test2 WHERE a > 1 ORDER BY 2;
+
+
+-- bug 114
+drop table if exists update_test cascade;
+CREATE TABLE update_test (a INT DEFAULT 10, b INT, c TEXT);
+INSERT INTO update_test VALUES (5, 10, 'foo');
+INSERT INTO update_test(b, a) VALUES (15, 10);
+UPDATE update_test t SET (a, b) = (SELECT b, a FROM update_test s WHERE s.a = t.a) WHERE CURRENT_USER = SESSION_USER;
+
+-- bug 121
+drop table if exists indext1 cascade;
+create table indext1(id integer);
+ALTER TABLE indext1 ADD CONSTRAINT oindext1_id_constraint UNIQUE (id);
+ALTER TABLE indext1 DROP CONSTRAINT oindext1_id_constraint;
+
+CREATE TABLE part1 (a serial primary key, b int, c varchar(32), unique (b,a)) PARTITION BY LIST (a);
+create index part1_b_c2 on part1(b,c);
+create table part1_0 partition of part1 for values in (1,2,3,4);
+
+insert into part1 (b,c) values(11, 'abc'),(12,'bcd'),(13,'cde');
+select*from part1;
+ALTER TABLE part1 ADD CONSTRAINT opart1_c_constraint UNIQUE (c,a);
+create table part1_1 partition of part1 for values in (5,6,7,8);
+insert into part1 (b,c) values(14, 'def'),(15,'efg'),(16,'fgh');
+alter table part1 add column d int not null;
+select*from part1;
+create table part1_2 partition of part1 for values in (9,10,11,12);
+insert into part1 (b,c,d) values(17, 'ghi', 21),(18,'hij',22),(19,'ijk',23);
+select*from part1;
+ALTER TABLE part1 ADD CONSTRAINT opart1_b_constraint UNIQUE (b,a);
+create table part1_3 partition of part1 for values in (13,14,15,16);
+insert into part1 (b,c,d) values(20, 'jkl', 24),(21,'klm',25),(22,'lmn',26);
+ALTER TABLE part1 ADD CONSTRAINT opart1_d_constraint UNIQUE (d,a);
+ALTER TABLE part1 DROP CONSTRAINT opart1_c_constraint;
+ALTER TABLE part1 DROP CONSTRAINT opart1_d_constraint;
+ALTER TABLE part1 DROP CONSTRAINT opart1_b_constraint;
+drop index part1_b_c2;
+drop table part1 cascade;
+
+
+-- bug 94
+ drop table if exists FLOAT4_TBL cascade;
+CREATE TABLE FLOAT4_TBL (f1 float4);
+INSERT INTO FLOAT4_TBL(f1) VALUES (' 0.0');
+INSERT INTO FLOAT4_TBL(f1) VALUES ('1004.30 ');
+INSERT INTO FLOAT4_TBL(f1) VALUES (' -34.84 ');
+INSERT INTO FLOAT4_TBL(f1) VALUES ('1.2345678901234e+20');
+INSERT INTO FLOAT4_TBL(f1) VALUES ('1.2345678901234e-20');
+select f1 from float4_tbl;
+SELECT f.* FROM FLOAT4_TBL f WHERE f.f1 = 1004.3;
+SELECT f.* FROM FLOAT4_TBL f WHERE f.f1 <> '1004.3';
+
+

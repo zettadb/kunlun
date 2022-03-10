e670578fd8c8bec2fbf91097f0b556e1b229e987
winter.zhang@live.com <winter.zhang@live.com>
2022-03-10 18:15:37 +0800

    [#445] copy the sql file back to overwrite original, to decrease the comparison diff

--- src/test/regress/runsuite_vs_officialpg.sh
+++ src/test/regress/runsuite_vs_officialpg.sh
@@ -12,7 +12,8 @@ cat serial_schedule | grep -v '^#' | sed '/^[ 	]*$/d' | awk '{print $2}' | while
 	else
 		echo "Running sql/$f.sql ......"
 		cat "sql/$f.sql" | grep -v DDL_STATEMENT > "$f.sql"
-		bash run.sh "$f.sql"
+		cp -f "$f.sql" "sql/$f.sql"
+		bash run.sh "sql/$f.sql"
 		mv out.diff $f.diff
 		mv out.diff.orig $f.diff.orig
 		mv 1.out $f.out1

adf326900930d24a0e7ff5567d60b9254a753954
vito4172 <vito@zettadb.com>
2023-02-13 13:59:39 +0800

    [#445] Use expecte out for pg_regress result checking jenkins 143

--- src/test/regress/sql/remote_dml3.sql
+++ src/test/regress/sql/remote_dml3.sql
@@ -359,4 +359,3 @@ select count(*) from join_foo
   left join (select b1.id, b1.t from join_bar b1 join join_bar b2 using (id)) ss
   on join_foo.id < ss.id + 1 and join_foo.id > ss.id - 1;
 
-set statement_timeout=0;
